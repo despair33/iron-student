7c3165d6180abba89da49bf2f338ac6213233bb0
AndreyMayorov <killmayorov8@gmail.com>
2026-04-21 03:04:23 +0300

    Update urls.py
    добавляю студентов список

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import students
 
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('', views.home, name='home'),
+    path('students/', students, name='students'),
 ]
