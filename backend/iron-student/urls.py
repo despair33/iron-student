f71b2c1fb25eb633c3ecb8b527b300b7c83440f5
stanislaw <example33@test.com>
2026-05-04 23:56:39 +0300

    Add route for photo static files

--- backend/iron-student/urls.py
+++ backend/iron-student/urls.py
@@ -37,3 +37,5 @@ urlpatterns = [
 if settings.DEBUG:
     # Статические файлы (CSS, JS, PDF) из frontend/static/
     urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR.parent / 'frontend/static')
+    # Фотографии участников из frontend/photo/
+    urlpatterns += static('photo/', document_root=settings.BASE_DIR.parent / 'frontend/photo')
